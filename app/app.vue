556ceb89188a234cef9e9e28ab2fd3b34578841a
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-02-15 19:17:25 +0100

    feat: typography classes

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,13 @@ const store = usePlayerStore()
 </script>
 
 <template>
-  <main class="px-4">
+  <main class="pt-4 px-4 sm:px-10">
+    <h1>heading 1</h1>
+    <h2>heading 2</h2>
+    <h3>heading 3</h3>
+    <h4>heading 4</h4>
+    <h5>heading 5</h5>
+    <h6>heading 6</h6>
     <Icon name="hugeicons:dart" />
     <Dialog>
       <DialogTrigger as-child>
