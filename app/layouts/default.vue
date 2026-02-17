5ab6a7a525951f184a996514248017ca73344596
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-02-17 08:34:07 +0100

    feat: added top and bottom slots to default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="flex min-h-dvh h-dvh flex-col">
-    <AppHeader class="shrink-0" />
+    <div class="shrink-0">
+      <AppHeader />
+      <div v-if="$slots.top" class="p-4 sm:px-10">
+        <slot name="top" />
+      </div>
+    </div>
 
     <main
       class="flex-1 overflow-y-auto py-4 px-4 sm:px-10 flex flex-col gap-4 items-start"
@@ -8,6 +13,11 @@
       <slot />
     </main>
 
-    <AppNavigation class="shrink-0" />
+    <div class="shrink-0">
+      <div v-if="$slots.bottom" class="p-4 sm:px-10">
+        <slot name="bottom" />
+      </div>
+      <AppNavigation />
+    </div>
   </div>
 </template>
