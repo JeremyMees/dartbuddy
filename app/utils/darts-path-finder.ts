38b898fed7a80031d1df6c040c6f16d76f1ebfa6
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-02-19 10:49:52 +0100

    feat: moved reusable static data to constants folder

--- app/utils/darts-path-finder.ts
+++ app/utils/darts-path-finder.ts
@@ -1,3 +1,5 @@
+import { boardNumbers } from '~~/constants/board-numbers'
+
 export interface DartThrow {
   segment: Segment
   score: number
@@ -24,10 +26,6 @@ interface BFSNode {
   completedTurns: CheckoutTurn[]
 }
 
-const BOARD_NUMBERS = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25,
-] as const
-
 function isFinishingDouble(dart: DartThrow): boolean {
   return dart.segment.startsWith('D')
 }
@@ -49,7 +47,7 @@ function makeDartThrow(number: number, multiplier: number): DartThrow | null {
 const throwsByScore: ReadonlyMap<number, DartThrow[]> = (() => {
   const map = new Map<number, DartThrow[]>()
 
-  for (const number of BOARD_NUMBERS) {
+  for (const number of boardNumbers) {
     const maxMultiplier = number === 25 ? 2 : 3
 
     for (let multiplier = 1; multiplier <= maxMultiplier; multiplier++) {
