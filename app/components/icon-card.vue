ebe9796a3b6c695b2d8cb04a92209bb06721bbda
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-02-19 13:34:42 +0100

    feat: created icon card component

---
+++ app/components/icon-card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{ icon: string }>()
+</script>
+
+<template>
+  <div
+    class="bg-primary/10 text-primary rounded-lg p-4 border border-primary flex items-center gap-6"
+  >
+    <div>
+      <Icon :name="icon" class="size-10!" />
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
