7c9dc5790c4e9ca23d9427710b639e3f1a9d7c17
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-02-20 15:22:07 +0100

    feat: created stat card component

---
+++ app/components/stat-card.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+defineProps<{
+  label: string
+  stat?: string | number
+}>()
+</script>
+
+<template>
+  <Card>
+    <CardHeader>
+      <span class="text-sm text-muted-foreground">{{ label }}</span>
+    </CardHeader>
+    <CardContent class="pt-0">
+      <slot>
+        <span class="text-3xl font-bold">
+          {{ stat }}
+        </span>
+      </slot>
+    </CardContent>
+  </Card>
+</template>
