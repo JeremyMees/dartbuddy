cc0ef2e74a86b956d52fc82717da3708af8aab22
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-02-20 11:54:13 +0100

    feat: pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
     'shadcn-nuxt',
     '@vueuse/nuxt',
+    '@vite-pwa/nuxt',
   ],
 
   devtools: { enabled: true },
@@ -14,6 +15,53 @@ export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
 
   compatibilityDate: '2025-07-15',
+
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'DartBuddy',
+      short_name: 'Buddy',
+      description: 'Dart scoring app',
+      theme_color: '#D97757',
+      background_color: '#262624',
+      display: 'standalone',
+      icons: [
+        {
+          src: '/web-app-manifest-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: '/web-app-manifest-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+  },
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-96x96.png',
+          sizes: '96x96',
+        },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+      ],
+      meta: [{ name: 'apple-mobile-web-app-title', content: 'Buddy' }],
+    },
+  },
 
   vite: {
     plugins: [
