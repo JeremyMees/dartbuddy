2bcc969f1063e2f1979584823ef0039c060e9edb
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-02-23 16:23:34 +0100

    fix: unit tests calculateSetsWon

---
+++ test/nuxt/utils/darts-utils/calculate-sets-won.spec.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest'
+import { calculateSetsWon } from '~/utils/darts-utils'
+import { playerOne, playerTwo, gameFull } from '~~/test/fixtures'
+
+describe('calculateSetsWon', () => {
+  it('returns the correct number of sets won by the player', () => {
+    const setsWonByPlayer1 = calculateSetsWon(gameFull, playerOne.id)
+    const setsWonByPlayer2 = calculateSetsWon(gameFull, playerTwo.id)
+
+    expect(setsWonByPlayer1).toBe(1)
+    expect(setsWonByPlayer2).toBe(0)
+  })
+})
