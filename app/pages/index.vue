0aafa37d66163536f4d258000dd9bc0fbbc282f2
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-03-31 09:10:10 +0200

    feat: error, pending and empty state for index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { gameTypes, gameRanges } from '#shared/constants/game'
-
 const selectedGameType = ref<GameType>('aroundTheClock')
 const selectedRange = ref<GameRange>('allTime')
 
@@ -15,20 +13,29 @@ const { data, pending, error } = useLazyFetch('/api/games', {
 
 <template>
   <NuxtLayout>
-    <div>hello world</div>
-    <pre>
-      error: {{ error }}
-    </pre>
-    <pre>
-      pending: {{ pending }}
-    </pre>
-    <pre>
-      gameTypes: {{ gameTypes }}
-    </pre>
-    <pre>
-      gameRanges: {{ gameRanges }}
-    </pre>
-    <pre>
+    <ErrorMessage v-if="error" :message="error.message" />
+
+    <div
+      v-else-if="pending"
+      class="flex flex-1 w-full items-center justify-center p-6 md:p-12"
+    >
+      <Spinner class="size-10!" />
+    </div>
+
+    <Empty v-else-if="!data?.games.length" class="w-full">
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <Icon name="hugeicons:dart" />
+        </EmptyMedia>
+        <EmptyTitle>No games found</EmptyTitle>
+        <EmptyDescription>
+          It looks like you haven't played any games yet. Start a game to add
+          your stats here!
+        </EmptyDescription>
+      </EmptyHeader>
+    </Empty>
+
+    <pre v-else-if="data.games.length">
       data: {{ data }}
     </pre>
   </NuxtLayout>
