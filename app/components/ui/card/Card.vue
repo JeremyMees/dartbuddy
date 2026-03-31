b8ff07e1b6ad65d99003b46b70830f06126ad71f
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-03-31 16:26:41 +0200

    feat: glowing card effect

--- app/components/ui/card/Card.vue
+++ app/components/ui/card/Card.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
     data-slot="card"
     :class="
       cn(
-        'bg-card flex flex-col gap-6 rounded-xl border py-4 shadow-sm',
+        'relative overflow-hidden bg-card flex flex-col gap-2 rounded-xl border py-4 shadow-sm',
         props.class,
       )
     "
