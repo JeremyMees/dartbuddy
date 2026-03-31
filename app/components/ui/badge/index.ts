1c3f3cc51a319fd182c08b2455f357612f869b76
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-03-31 15:33:47 +0200

    feat: updated badge styles

--- app/components/ui/badge/index.ts
+++ app/components/ui/badge/index.ts
@@ -9,11 +9,11 @@ export const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          'border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90',
+          'border-transparent bg-primary/20 text-primary [a&]:hover:bg-primary/10 focus-visible:ring-primary/10',
         secondary:
-          'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
+          'border-transparent bg-secondary/20 text-secondary [a&]:hover:bg-secondary/10 focus-visible:ring-secondary/10',
         destructive:
-          'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+          'border-transparent bg-destructive/20 text-destructive [a&]:hover:bg-destructive/10 focus-visible:ring-destructive/10',
         outline:
           'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
       },
