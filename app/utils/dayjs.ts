fdb183dbe770983f3e1c0cad5e62e6f3a95999b5
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-03-31 16:34:33 +0200

    feat: added formatReadDate util function

--- app/utils/dayjs.ts
+++ app/utils/dayjs.ts
@@ -7,6 +7,10 @@ export function formatDate(date: Date | string) {
   return dayjs(date).format('DD/MM/YY')
 }
 
+export function formatReadDate(date: Date | string) {
+  return dayjs(date).format('DD MMM, HH:mm')
+}
+
 export function timeAgo(date: Date | string, withoutSuffix?: boolean) {
   return dayjs(date).fromNow(withoutSuffix)
 }
