11bc6da7664a5b36a3b2ef0b3ba2df41dd4fee4a
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-04-09 10:25:34 +0200

    feat: number ticker component

---
+++ app/components/ui/number-ticker/NumberTicker.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { cn } from '@inspira-ui/plugins'
+import {
+  TransitionPresets,
+  // useElementVisibility,
+  // useTransition,
+} from '@vueuse/core'
+
+type TransitionsPresetsKeys = keyof typeof TransitionPresets
+
+const props = withDefaults(
+  defineProps<{
+    value?: number
+    direction?: 'up' | 'down'
+    duration?: number
+    delay?: number
+    decimalPlaces?: number
+    class?: string
+    transition?: TransitionsPresetsKeys
+  }>(),
+  {
+    value: 0,
+    direction: 'up',
+    delay: 0,
+    duration: 1000,
+    decimalPlaces: 2,
+    transition: 'easeOutCubic',
+    class: '',
+  },
+)
+
+const spanRef = ref<HTMLSpanElement>()
+
+const transitionValue = ref(props.direction === 'down' ? props.value : 0)
+
+const transitionOutput = useTransition(transitionValue, {
+  delay: props.delay,
+  duration: props.duration,
+  transition: TransitionPresets[props.transition],
+})
+
+const output = computed(() => {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: props.decimalPlaces,
+  }).format(Number(transitionOutput.value.toFixed(props.decimalPlaces)))
+})
+
+const isInView = useElementVisibility(spanRef, {
+  threshold: 0,
+})
+
+const hasBeenInView = ref(false)
+
+const stopIsInViewWatcher = watch(
+  isInView,
+  (isVisible) => {
+    if (isVisible && !hasBeenInView.value) {
+      hasBeenInView.value = true
+      transitionValue.value = props.direction === 'down' ? 0 : props.value
+      stopIsInViewWatcher()
+    }
+  },
+  { immediate: true },
+)
+
+watch(
+  () => props.value,
+  (newVal) => {
+    if (hasBeenInView.value) {
+      transitionValue.value = props.direction === 'down' ? 0 : newVal
+    }
+  },
+)
+</script>
+
+<template>
+  <span
+    ref="spanRef"
+    :class="cn(`inline-block tracking-wider tabular-nums`, props.class)"
+  >
+    {{ output }}
+  </span>
+</template>
