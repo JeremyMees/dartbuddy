3ce086739c9ac23df11b9d91e19a6092d61c3e20
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-04-10 15:13:21 +0200

    feat: custom error page

---
+++ app/error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{ error: NuxtError }>()
+</script>
+
+<template>
+  <div class="px-4 py-4 sm:px-10 flex flex-col gap-6 items-center">
+    <h1>{{ error.status }}</h1>
+    <p>
+      {{ error.message }}
+    </p>
+    <div
+      class="text-xs text-muted-foreground border rounded p-2 bg-muted max-w-full overflow-x-auto whitespace-pre"
+    >
+      {{ error.stack }}
+    </div>
+  </div>
+</template>
